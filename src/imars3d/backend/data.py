1a3fd984e13cd0ef159eb95fbb462626a50aff4e
Chen Zhang <zhangc@ornl.gov>
2022-09-21 14:56:39 -0400

    add load images

--- src/imars3d/backend/data.py
+++ src/imars3d/backend/data.py
@@ -4,7 +4,9 @@ Data handling for imars3d.
 """
 import param
 import numpy as np
+import dxchange
 from functools import partial
+from pathlib import Path
 from typing import Optional, Tuple, List, Callable
 from tqdm.contrib.concurrent import process_map
 
@@ -12,6 +14,48 @@ from tqdm.contrib.concurrent import process_map
 # setup module level logger
 logger = param.get_logger()
 logger.name = __name__
+
+
+# use _func to avoid sphinx pulling it into docs
+def _load_images(
+    filelist: List[str],
+    desc: str,
+    max_workers: int,
+) -> np.ndarray:
+    """
+    Load image data via dxchange.
+    
+    Parameters
+    ----------
+    filelist:
+        List of images filenames/path for loading via dxchange.
+    desc:
+        Description for progress bar.
+    max_workers:
+        Maximum number of processes allowed during loading.
+
+    Returns
+    -------
+        Image array stack.
+    """
+    # figure out the file type and select corresponding reader from dxchange
+    file_ext = Path(filelist[0]).suffix.lower()
+    if file_ext in (".tif", ".tiff"):
+        reader = dxchange.read_tiff
+    elif file_ext == ".fits":
+        reader = dxchange.read_fits
+    else:
+        logger.error(f"Unsupported file type: {file_ext}")
+        raise ValueError("Unsupported file type.")
+    # read the data into numpy array via map_process
+    rst = process_map(
+        partial(_forgiving_reader, reader=reader),
+        filelist,
+        max_workers=max_workers,
+        desc=desc,
+    )
+    # return the results
+    return np.array([me for me in rst if me is not None])
 
 
 # use _func to avoid sphinx pulling it into docs
--- tests/unit/backend/test_data.py
+++ tests/unit/backend/test_data.py
@@ -2,10 +2,43 @@
 """
 Unit tests for backend data loading.
 """
+import os
 import pytest
-from unittest import mock
+import astropy.io.fits as fits
+import dxchange
 import numpy as np
+from functools import partial
 from imars3d.backend.data import _forgiving_reader
+from imars3d.backend.data import _load_images
+
+
+@pytest.fixture(scope="module")
+def test_data():
+    # create some test data
+    data = np.ones((3, 3))
+    dxchange.write_tiff(data, "test")
+    hdu = fits.PrimaryHDU(data)
+    hdu.writeto('test.fits')
+    yield
+    # remove the test data
+    os.remove("test.tiff")
+    os.remove("test.fits")
+
+
+def test_load_images(test_data):
+    func = partial(_load_images, desc="test", max_workers=2)
+    # error case: unsupported file format
+    incorrect_filelist = ["file1.bad", "file2.bad"]
+    with pytest.raises(ValueError):
+        rst = func(filelist=incorrect_filelist)
+    # case1: tiff
+    tiff_filelist = ["test.tiff", "test.tiff"]
+    rst = func(filelist=tiff_filelist)
+    assert rst.shape == (2, 3, 3)
+    # case2: fits
+    fits_filelist = ["test.fits", "test.fits"]
+    rst = func(filelist=fits_filelist)
+    assert rst.shape == (2, 3, 3)
 
 
 def test_forgiving_reader():
